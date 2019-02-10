15b832e3fea9a23b42c77e68c405cafa10403723
Masato Hatayama <masato.hatayama@gmail.com>
2019-02-10 19:49:24 +0900

    検索結果のアイテム名をクリックして更に検索できる機能

--- public/js/app.js
+++ public/js/app.js
@@ -10,13 +10,13 @@ qlForm.addEventListener("submit", getSearchResult);
 function getSearchResult(e) {
   e.preventDefault();
 
-  elMxingResult.innerHTML = "";
-
   const searchText = elSearchTextInput.value;
 
   if (searchText === "") {
     return;
   }
+
+  elMxingResult.innerHTML = "";
 
   fetch(`/api/search/${encodeURI(searchText)}`)
     .then(res => {
@@ -38,14 +38,31 @@ function getSearchResult(e) {
       elMxingResult.innerHTML = json.map(row => {
         return `<tr>
           <td>${row.category}</td>
-          <td>${row.result}</td>
-          <td>${row.item1}</td>
-          <td>${row.item2}</td>
-          <td>${row.item3}</td>
-          <td>${row.item4}</td>
-          <td>${row.item5}</td>
-          <td>${row.item6}</td>
+          <td class="click-search">${row.result}</td>
+          <td class="click-search">${row.item1}</td>
+          <td class="click-search">${row.item2}</td>
+          <td class="click-search">${row.item3}</td>
+          <td class="click-search">${row.item4}</td>
+          <td class="click-search">${row.item5}</td>
+          <td class="click-search">${row.item6}</td>
           </tr>`
       }).join("\n");
+
+      document.querySelectorAll("td.click-search").forEach(item => {
+        item.addEventListener("click", instantClickSearch);
+      });
     });
+}
+
+function instantClickSearch(e) {
+  let searchText = e.target.innerHTML;
+  if (searchText.includes("<br>")) {
+    searchText = searchText.split("<br>")[0];
+  }
+  if (/(.+)\(\d+\)$/.test(searchText)) {
+    searchText = /(.+)\(\d+\)$/.exec(searchText)[1];
+  }
+
+  elSearchTextInput.value = searchText;
+  getSearchResult(e);
 }
